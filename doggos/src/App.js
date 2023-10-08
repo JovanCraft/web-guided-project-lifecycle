b73730c6418383dbacd6f74d6615a60efef80185
jovan <jovancraft@gmail.com>
2023-10-08 13:30:01 -0500

    wrote my axios fetch request

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import axios from 'axios';
+
+const fetchDoggos = breed => {
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+    .then(res => res)
+    .catch(err => console.error('nooooo!'))
+}
+
+class App extends React.Component{
+    constructor(){
+        console.log('constructor ran')
+        super();
+        this.state = {
+            doggos: [],
+            breed: ''
+        }
+    }
+
+    render() {
+        console.log('render function ran')
+        return (
+            <>
+                <h1>My App</h1>
+            </>
+        )
+    }
+}
+
+export default App;
